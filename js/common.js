7ec278a9473419dd4f0674a9c60536bbdf2c4271
FrontendMetis <rustamvalyaev@yandex.ru>
2018-06-03 13:51:26 +0300

    13:51

--- js/common.js
+++ js/common.js
@@ -14,16 +14,34 @@ function deleteClass(ell) {
 	}
 }
 
-window.onload = function() {
+// Particles main
+particlesJS.load('particles', 'libs/particles.js-master/package.json');
+
+// Type text
+var typed = new Typed('.js-type', {
+	strings: ["Html верстальщик", "Junior Front-end"],
+	typeSpeed: 100,
+	loop: true,
+	backDelay: 1200,
+	backSpeed: 20
+});
+
+
+window.addEventListener('load', function() {
+	// Remove preloader
+	console.log('loaded');
+	let preloader = document.querySelector('.loading');
+	setTimeout(function() { preloader.parentNode.removeChild(preloader); }, 2500);
+
 	// Btn mobile toggle nuv
-	var mobileBtn = document.getElementById('btn_show_nuv');
+	let mobileBtn = document.getElementById('btn_show_nuv');
 	mobileBtn.addEventListener('click', function(el) {
 		el.currentTarget.classList.toggle('btn_show_nuv__active');
 		document.querySelector('.nuv_block_active').classList.toggle('nuv_block_active__mobile');
 	});
 
 	// Navigation
-	var nuvEll = document.querySelectorAll('.nuv > li > a'); 
+	let nuvEll = document.querySelectorAll('.nuv > li > a'); 
 	for(let el = 0; el < nuvEll.length; el++) {
 		nuvEll[el].addEventListener('click', function(e) {
 			deleteClass('.nuv > li');
@@ -45,25 +63,13 @@ window.onload = function() {
 	}
 
 	// Switch nuv
-	var btnNuvSwitch = document.querySelector('.js-switch_nuv');
+	let btnNuvSwitch = document.querySelector('.js-switch_nuv');
 	btnNuvSwitch.addEventListener('click', function(e) {
 		e.target.parentNode.classList.toggle('nuv_block');
 	});
 
-	// Particles main
-	particlesJS.load('particles', '../libs/particles.js-master/package.json');
-
-	// Type text
-	var typed = new Typed('.js-type', {
-		strings: ["Html верстальщик", "Junior Front-end"],
-		typeSpeed: 100,
-		loop: true,
-		backDelay: 1200,
-		backSpeed: 20
-	});
-
 	// Link portfolio
-	var linkPortfolio = document.getElementById('banner_portfolio');
+	let linkPortfolio = document.getElementById('banner_portfolio');
 	linkPortfolio.addEventListener('click', function(e) {
 		// Toggle class
 		deleteClass('.nuv > li');
@@ -83,7 +89,7 @@ window.onload = function() {
 	});
 
 	// TAB About
-	var tabElLnk = document.querySelectorAll('.about_tab__nuv > ul > li > a');
+	let tabElLnk = document.querySelectorAll('.about_tab__nuv > ul > li > a');
 		for(let el = 0; el < tabElLnk.length; el++) {
 		tabElLnk[el].addEventListener('click', function(e) {
 			e.preventDefault();
@@ -98,4 +104,4 @@ window.onload = function() {
 			document.querySelector(takeAttr).style.display = 'block';
 		});
 	}
-}
+})
