cdc34c3e2eddcbc1608ef36a864e3db27788a688
FrontendMetis <rustamvalyaev@yandex.ru>
2018-06-23 16:40:50 +0300

    16:41

--- js/common.js
+++ js/common.js
@@ -1,10 +1,33 @@
 $(document).ready(function() {
+	// Pressed header
+	var takeH = $('.header').innerHeight();
+	$(window).scroll(function() {
+		if($(window).innerWidth() < 992) {
+			var currentScroll = $(window).scrollTop();
+			if(currentScroll > takeH) {
+				$('.header_logo > img').css('height', '25px');
+			} else {
+				$('.header_logo > img').css('height', '30px');
+			}
+		}
+	});
+
 	// Search modal
 	$('.header_search').click(function() {
 		$('.popup_search').fadeIn();
 	});
 	$('.popup_search_close').click(function() {
 		$('.popup_search').fadeOut();
+	});
+
+	// Mobile menu
+	$('.to_mobile_menu').click(function() {
+		$('.popup_mobile_menu').fadeIn().addClass('popup_mobile_menu__active');
+		$('body').css('overflow-y', 'hidden');
+	});
+	$('.close_mobile_menu, .popup_mobile_menu .mask').click(function() {
+		$('.popup_mobile_menu').removeClass('popup_mobile_menu__active').fadeOut();
+		$('body').css('overflow-y', 'scroll');
 	});
 
 	// Masonry
